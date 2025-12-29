6e5308e947ba49b612af63e6c671d6e86539ffac
luiz_ <luiz_carvalho20@hotmail.com>
2025-12-29 10:13:05 -0300

    feat: cria connectionfactory e testa conexão

--- src/main/java/br/com/dio/board/Main.java
+++ src/main/java/br/com/dio/board/Main.java
@@ -1,7 +1,15 @@
 package br.com.dio.board;
 
+import br.com.dio.board.db.ConnectionFactory;
+
+import java.sql.Connection;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Projeto Board iniciado!");
+        try (Connection con = ConnectionFactory.getConnection()) {
+            System.out.println("Conectado no MySQL!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
